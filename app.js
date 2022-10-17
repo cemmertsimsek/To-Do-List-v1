15404ceec48a16177e4dc88590f500c5b2cf6571
cem <cemsimseksm@gmail.com>
2022-10-17 19:45:00 +0300

    mutliple work spaces connected to app.js

--- app.js
+++ app.js
@@ -3,40 +3,50 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+let workItems = [];
+
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-      break;
+app.get("/", function (req, res) {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
   }
-  res.render("List", { kindOfDay: day });
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function () {
